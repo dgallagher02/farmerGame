4f47174b3b38b205890708503f7d85e4d860bb5a
DanGallagher <dgallagher2013@gmail.com>
2023-11-24 12:46:26 -0500

    Initial Commit

---
+++ models.py
@@ -0,0 +1,11 @@
+
+
+
+from django.db import models
+from django.contrib.auth.models import User
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+class GameData(models.Model):
+    user_profile = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
